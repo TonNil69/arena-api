5e5c5fe77abe33483df752615913ba73d2e44d19
Nilton Soares <Nilton.neto@dotgroup.com.br>
2019-07-11 15:12:43 -0300

    Implementar SPA

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-var router = require('./router/router')
+var router = require('./src/router/router')
 require('dotenv').config()
 
 app.use('/', router)
