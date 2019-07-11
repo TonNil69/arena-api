a70b5bc878bfc54f591e0d981b3eb5b7f5cda019
Breno Mazieiro <breno.mazieiro@dotgroup.com.br>
2019-07-11 12:50:21 -0300

    Refactoring and improving docker-compose

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-var router = require('./src/router/router')
+var router = require('./router/router')
 require('dotenv').config()
 
 app.use('/', router)
